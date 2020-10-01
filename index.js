5dc35421a69c21ba03f82d00a842804f75d66964
Murad Hossain <sliverboymurad@gmail.com>
2020-10-01 09:21:30 +0600

    port updated and nodemon fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
 
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
@@ -19,12 +20,51 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 client.connect((err) => {
-  const products = client.db('emaJohnStore').collection('products');
+  const productsCollection = client.db('emaJohnStore').collection('products');
+  const ordersCollection = client.db('emaJohnStore').collection('orders');
 
+  //Add all data from fakedata api
   app.post('/addProduct', (req, res) => {
-    const product = req.body;
-    products.insertOne(product).then((result) => {
-      console.log(result);
+    const products = req.body;
+
+    productsCollection.insertMany(products).then((result) => {
+      console.log(res.send(result.insertedCount));
+      res.send(result.insertedCount);
+    });
+  });
+
+  //Read data from server - all products
+  app.get('/products', (req, res) => {
+    productsCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  // load single product
+  app.get('/product/:key', (req, res) => {
+    productsCollection
+      .find({ key: req.params.key })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
+  });
+
+  // load some specific products
+  app.post('/productsByKeys', (req, res) => {
+    const productKeys = req.body;
+    productsCollection
+      .find({ key: { $in: productKeys } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  // Order information
+  app.post('/addOrder', (req, res) => {
+    const order = req.body;
+
+    ordersCollection.insertOne(order).then((result) => {
+      res.send(result.insertedCount > 0);
     });
   });
 });
